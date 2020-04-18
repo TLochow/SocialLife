37a466a5ff7f6e2660798821e5e3db30f6bc013c
Torben Lochow <t.lochow@gmx.de>
2020-04-18 18:21:51 +0200

    Events defined.

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -10,6 +10,8 @@ var Social = 50.0
 
 var SelectedPaddle = 0
 
+var EventSpawnTime = 5.0
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	$Paddles/RedPaddle.SetPaddleType(1)
@@ -51,14 +53,18 @@ func _process(delta):
 	if mousePos.distance_to(Vector2(0, 0)) > 30.0:
 		get_viewport().warp_mouse(Vector2(400, 400) - (mouseDir.normalized() * 30.0))
 	
-	Energy = clamp(Energy - (1.0 * delta), 0.0, 100.0)
-	Fitness = clamp(Fitness - (1.0 * delta), 0.0, 100.0)
-	Social = clamp(Social - (1.0 * delta), 0.0, 100.0)
+	var malus = 0.1 * delta
+	Energy = clamp(Energy - malus, 0.0, 100.0)
+	Fitness = clamp(Fitness - malus, 0.0, 100.0)
+	Social = clamp(Social - malus, 0.0, 100.0)
 	$Camera2D/UI/Control/Scores/EnergyBar.value = Energy
 	$Camera2D/UI/Control/Scores/FitnessBar.value = Fitness
 	$Camera2D/UI/Control/Scores/SocialBar.value = Social
 	
 	$Camera2D/UI/Control/Score.text = str(Score)
+	
+	EventSpawnTime = max(EventSpawnTime - (0.01 * delta), 0.5)
+	$EventSpawnTimer.wait_time = EventSpawnTime
 
 func _on_EventSpawnTimer_timeout():
 	SpawnNewEvent()
@@ -71,28 +77,26 @@ func SpawnNewEvent():
 	event.set_position(pos)
 	event.set_linear_velocity(pos.normalized() * -80.0)
 	
-	var eventType = randi() % 6
+	var eventType = randi() % 9
 	match eventType: #                       Fit, Energy, Social
 		0: # Bed
 			event.SetEventType(3, eventType, 0.0, 30.0, -15.0)
-		1: # Shower
-			event.SetEventType(2, eventType, 10.0, 0.0, -5.0)
+		1: # Apple
+			event.SetEventType(2, eventType, 15.0, 0.0, 0.0)
 		2: # Bar
-			event.SetEventType(1, eventType, 0.0, -15.0, 30.0)
+			event.SetEventType(1, eventType, -15.0, 0.0, 30.0)
 		3: # Energy Drink
-			event.SetEventType(3, eventType, -20.0, 0, 30.0)
+			event.SetEventType(3, eventType, -15.0, 30.0, 0.0)
 		4: # GYM
-			event.SetEventType(2, eventType, 30.0, -20.0, 0.0)
+			event.SetEventType(2, eventType, 30.0, -15.0, 0.0)
 		5: # Talk
-			event.SetEventType(1, eventType, 0, -5.0, 30.0)
-		6: 
-			event.SetEventType(0, eventType, 0, 0, 0)
-		7: 
-			event.SetEventType(0, eventType, 0, 0, 0)
-		8:
-			event.SetEventType(0, eventType, 0, 0, 0)
-		9:
-			event.SetEventType(0, eventType, 0, 0, 0)
+			event.SetEventType(1, eventType, 0, 0.0, 15.0)
+		6: # Concert
+			event.SetEventType(1, eventType, 0, -15.0, 30.0)
+		7: # Coffee
+			event.SetEventType(3, eventType, 0, 15.0, 0)
+		8: # Meditation
+			event.SetEventType(2, eventType, 30.0, 0, -15.0)
 	
 	event.connect("Impact", self, "onEventImpact")
 	
